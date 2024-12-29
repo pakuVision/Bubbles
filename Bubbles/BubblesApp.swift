2ddbaa726839b25d18c71c7f3ac67cc65be12d36
pakuVision <boardguy.vision@gmail.com>
2024-12-29 23:20:48 +0900

    Initial Commit

---
+++ Bubbles/BubblesApp.swift
@@ -0,0 +1,33 @@
+//
+//  BubblesApp.swift
+//  Bubbles
+//
+//  Created by boardguy.vision on 2024/12/29.
+//
+
+import SwiftUI
+
+@main
+struct BubblesApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
